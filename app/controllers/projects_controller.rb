3c1fe44d0c88200db670c3312436a1487199374f
ewelinaszoda <ewelinaszoda@gmail.com>
2020-07-23 12:45:08 +0100

    auth done

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,10 +1,35 @@
 class ProjectsController < ApplicationController
-  def index
-  end
 
-  def create
+  def index
+    projects = Project.all
+    render json: { projects: projects } 
   end
 
   def show
+    project = Project.find_by(params[:id])
+    render json: { projects: project }
   end
+
+  def create
+    project = Project.create(project_params)
+  end
+
+  private
+
+  def project_params
+      params.require(:project).permit(
+        :name, 
+        :address,
+        :image, 
+        :description, 
+        :client,
+        :project_manager,
+        :site_manager,
+        :quantity_surveyor,
+        :start_date,
+        :end_date,
+        :user_id,
+      )
+  end
+
 end
