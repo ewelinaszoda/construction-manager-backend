843e4ed4321fff2320fad1981fbd70806a399623
ewelinaszoda <ewelinaszoda@gmail.com>
2020-08-15 12:50:13 +0100

    change database from sqlite3 to Postgresgl

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_01_105805) do
+ActiveRecord::Schema.define(version: 2020_07_17_140709) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "drawings", force: :cascade do |t|
     t.string "title"
